29c48de955616ad6ef5b7244e49be47b24d157f2
Sahil-Hode <sahilhode67@gmail.com>
2026-07-26 12:00:00 +0530

    feat: implement SectionRouteObserver to sync URL hash with active viewport section on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import { Navbar } from "@/components/navbar";
 import { Hero } from "@/components/hero";
 import { About } from "@/components/about";
 import { GlobalFloatingProduct } from "@/components/GlobalFloatingProduct";
+import { SectionRouteObserver } from "@/components/SectionRouteObserver";
 
 export default function Home() {
   return (
     <div className="min-h-screen flex flex-col bg-[#FAF8F5] relative">
+      <SectionRouteObserver />
       <GlobalFloatingProduct />
       <Navbar />
       <main className="flex-1 flex flex-col">
