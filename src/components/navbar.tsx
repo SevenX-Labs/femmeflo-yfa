31047ef2f53a64d8b3aa449689732d1fd4e947e4
Sahil-Hode <sahilhode67@gmail.com>
2026-08-12 23:43:51 +0530

    feat: implement responsive Navbar component with logo and scroll-aware navigation links

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -50,7 +50,7 @@ export function Navbar() {
           {/* 1. Left: Brand Logo */}
           <Link href="/" className="flex items-center group shrink-0 cursor-pointer">
             <Image
-              src="/femmeflo.png"
+              src="/logo.png"
               alt="Femmeflo Logo"
               width={600}
               height={200}
