263d67746194f4bc7045a0cf42946e8c9d16fe2d
Sahil-Hode <sahilhode67@gmail.com>
2026-08-12 19:15:29 +0530

    feat: implement responsive Navbar component with asset integration

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -54,7 +54,7 @@ export function Navbar() {
               alt="Femmeflo Logo"
               width={600}
               height={200}
-              className="h-11 sm:h-14 lg:h-[70px] xl:h-[80px] w-auto object-contain transition-transform duration-300 group-hover:scale-[1.03]"
+              className="h-11 sm:h-14 lg:h-[72px] xl:h-[84px] w-auto object-contain transition-transform duration-300 group-hover:scale-[1.03]"
               priority
             />
           </Link>
